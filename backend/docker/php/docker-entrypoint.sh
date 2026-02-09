5b30850d39ea322a6f28444a8c19ff01b1622b6f
CelsiusIV <celiasp17@gmail.com>
2026-02-09 21:57:39 +0100

    modificado documentacion docker

--- backend/docker/php/docker-entrypoint.sh
+++ backend/docker/php/docker-entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'artisan' ]; then
-	php -r "file_exists('.env') || copy('.env.example', '.env');"
-	composer install --prefer-dist --no-progress --no-interaction
-    php artisan migrate --no-interaction
+# Crear .env si no existe
+if [ ! -f .env ]; then
+    cp .env.example .env
 fi
+
+# Instalar dependencias solo si no existe vendor/
+if [ ! -d vendor ]; then
+    composer install --prefer-dist --no-progress --no-interaction
+fi
+
+if [ "$MIGRATE" = "true" ]; then
+    php artisan migrate --force
+fi
+
+
+#if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'artisan' ]; then
+	#php -r "file_exists('.env') || copy('.env.example', '.env');"
+#	composer install --prefer-dist --no-progress --no-interaction
+    #php artisan migrate --no-interaction
+#fi
 
 exec docker-php-entrypoint "$@"
